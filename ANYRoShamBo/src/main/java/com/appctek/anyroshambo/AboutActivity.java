dea97f52528585b1bf705afb5427a42dc959e6c9
acc15 <vyacheslav.mayorov@gmail.com>
2013-12-28 23:31:28 +0400

    Creates AboutActivity

--- ANYRoShamBo/src/main/java/com/appctek/anyroshambo/AboutActivity.java
+++ ANYRoShamBo/src/main/java/com/appctek/anyroshambo/AboutActivity.java
@@ -1,10 +1,18 @@
 package com.appctek.anyroshambo;
 
 import android.app.Activity;
+import android.os.Bundle;
+import com.appctek.R;
 
 /**
  * @author Vyacheslav Mayorov
  * @since 2013-28-12
  */
 public class AboutActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
 }
