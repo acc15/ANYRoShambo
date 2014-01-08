1ff1d7985d93c2637c0abac76cd4264b52b133d1
acc15 <vyacheslav.mayorov@gmail.com>
2014-01-08 18:49:19 +0400

    Added new source root

---
+++ ANYRoShamBo/src/ad/java/com/appctek/StartAppAdService.java
@@ -0,0 +1,13 @@
+package com.appctek;
+
+import com.appctek.anyroshambo.services.AdService;
+
+/**
+ * @author Vyacheslav Mayorov
+ * @since 2014-08-01
+ */
+public class StartAppAdService implements AdService {
+
+
+
+}
---
+++ ANYRoShamBo/src/main/java/com/appctek/anyroshambo/services/AdService.java
@@ -0,0 +1,8 @@
+package com.appctek.anyroshambo.services;
+
+/**
+ * @author Vyacheslav Mayorov
+ * @since 2014-08-01
+ */
+public interface AdService {
+}
