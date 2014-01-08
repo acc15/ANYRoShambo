9c6ff73ea6daacc6c3d766afb90ed9ee3aaa1503
acc15 <vyacheslav.mayorov@gmail.com>
2014-01-08 07:08:15 +0400

    Added URLs in about screen

--- ANYRoShamBo/src/main/java/com/appctek/anyroshambo/AboutActivity.java
+++ ANYRoShamBo/src/main/java/com/appctek/anyroshambo/AboutActivity.java
@@ -2,6 +2,7 @@ package com.appctek.anyroshambo;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.TextView;
@@ -21,6 +22,11 @@ public class AboutActivity extends FullScreenActivity {
         final Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/myriadpro.ttf");
         ((TextView)findViewById(R.id.info_header)).setTypeface(typeface);
         ((TextView)findViewById(R.id.info_text)).setTypeface(typeface);
+
+        ((TextView)findViewById(R.id.main_url)).setMovementMethod(LinkMovementMethod.getInstance());
+        ((TextView)findViewById(R.id.vk_url)).setMovementMethod(LinkMovementMethod.getInstance());
+        ((TextView)findViewById(R.id.tw_url)).setMovementMethod(LinkMovementMethod.getInstance());
+        ((TextView)findViewById(R.id.ig_url)).setMovementMethod(LinkMovementMethod.getInstance());
 
         final View container = findViewById(android.R.id.content);
         final View rootView = findViewById(R.id.about_screen);
