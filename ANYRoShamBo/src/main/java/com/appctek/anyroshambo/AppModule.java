2a741fcf441dee72551a850b5d482a7d7c58e227
acc15 <vyacheslav.mayorov@gmail.com>
2014-01-12 04:40:23 +0400

    Fixed AppModule

--- ANYRoShamBo/src/main/java/com/appctek/anyroshambo/AppModule.java
+++ ANYRoShamBo/src/main/java/com/appctek/anyroshambo/AppModule.java
@@ -13,13 +13,18 @@ public class AppModule extends AbstractModule {
 
     public static final String AD_SERVICE_CLASS = "com.appctek.anyroshambo.services.StartAppAdService";
 
+    private static Class<?> getClassOrDefault(String className, Class<?> defaultClass) {
+        try {
+            return Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            return defaultClass;
+        }
+    }
+
     @Override
     protected void configure() {
         bind(AppInfo.class).toInstance(new AppInfo(AppModule.class));
-        try {
-            bind(AdService.class).to(Class.forName(AD_SERVICE_CLASS).asSubclass(AdService.class)).in(Singleton.class);
-        } catch (ClassNotFoundException e) {
-            bind(AdService.class).to(NoAdService.class).in(Singleton.class);
-        }
+        bind(AdService.class).to(getClassOrDefault(AD_SERVICE_CLASS, NoAdService.class).
+                asSubclass(AdService.class)).in(Singleton.class);
     }
 }
