ad22eaabe2daf2924a4d7937c36f1bcc3140096c
acc15 <vyacheslav.mayorov@gmail.com>
2014-01-26 17:58:24 +0400

    Native share dialog removed

--- ANYRoShamBo/src/main/java/com/appctek/anyroshambo/social/FacebookService.java
+++ ANYRoShamBo/src/main/java/com/appctek/anyroshambo/social/FacebookService.java
@@ -4,9 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import com.appctek.anyroshambo.roboguice.OnSaveInstanceStateEvent;
 import com.appctek.anyroshambo.social.auth.ErrorInfo;
-import com.appctek.anyroshambo.util.Action;
 import com.facebook.*;
-import com.facebook.widget.FacebookDialog;
 import com.facebook.widget.WebDialog;
 import com.google.inject.Inject;
 import org.slf4j.Logger;
@@ -26,20 +24,6 @@ public class FacebookService implements SocialNetworkService {
 
     private UiLifecycleHelper uiHelper;
 
-    private Action<ErrorInfo> lastFinishAction; // state for tracking last share call result
-
-    private void executeLastFinishAction(ErrorInfo errorInfo) {
-        if (lastFinishAction == null) {
-            return;
-        }
-        try {
-            lastFinishAction.execute(errorInfo);
-        } finally {
-            lastFinishAction = null;
-        }
-    }
-
-
     public static enum Error {
         FEED_ERROR
     }
@@ -55,16 +39,7 @@ public class FacebookService implements SocialNetworkService {
     }
 
     public void onActivityResult(@Observes OnActivityResultEvent event) {
-        uiHelper.onActivityResult(event.getRequestCode(), event.getResultCode(), event.getData(), new FacebookDialog.Callback() {
-
-            public void onComplete(FacebookDialog.PendingCall pendingCall, Bundle data) {
-                executeLastFinishAction(createErrorInfoByNativeDialogComplete(data));
-            }
-
-            public void onError(FacebookDialog.PendingCall pendingCall, Exception error, Bundle data) {
-                executeLastFinishAction(createErrorInfoByException(Error.FEED_ERROR, error));
-            }
-        });
+        uiHelper.onActivityResult(event.getRequestCode(), event.getResultCode(), event.getData());
     }
 
     public void onResume(@Observes OnResumeEvent event) {
@@ -81,16 +56,6 @@ public class FacebookService implements SocialNetworkService {
 
     public void onDestroy(@Observes OnDestroyEvent event) {
         uiHelper.onDestroy();
-    }
-
-    private ErrorInfo createErrorInfoByNativeDialogComplete(Bundle data) {
-        final String gesture = FacebookDialog.getNativeDialogCompletionGesture(data);
-        final String postId = FacebookDialog.getNativeDialogPostId(data);
-        if (postId != null || "post".equals(gesture)) {
-            return ErrorInfo.success();
-        } else {
-            return ErrorInfo.create(CommonError.USER_CANCELLED);
-        }
     }
 
     private ErrorInfo createErrorInfoByException(Enum<?> defaultErrorCode, Exception ex) {
@@ -112,26 +77,13 @@ public class FacebookService implements SocialNetworkService {
 
                 if (session.isOpened()) {
 
-                    // There i a bug with native share dialog: https://developers.facebook.com/x/bugs/804864539527432/
-                    if (FacebookDialog.canPresentShareDialog(activity, FacebookDialog.ShareDialogFeature.SHARE_DIALOG)) {
-                        final FacebookDialog.PendingCall pendingCall = new FacebookDialog.ShareDialogBuilder(activity).
-                                setName(shareParams.getTitle()).
-                                setDescription(shareParams.getText()).
-                                setLink(shareParams.getLink()).
-                                build().present();
-                        lastFinishAction = shareParams.getFinishAction();
-                        uiHelper.trackPendingDialogCall(pendingCall);
-                        return;
-                    }
-
                     final Bundle params = new Bundle();
                     params.putString("name", shareParams.getTitle());
                     params.putString("description", shareParams.getText());
                     params.putString("link", shareParams.getLink());
                     new WebDialog.FeedDialogBuilder(activity, session, params).
                             setOnCompleteListener(new WebDialog.OnCompleteListener() {
-                                public void onComplete(Bundle values,
-                                                       FacebookException error) {
+                                public void onComplete(Bundle values, FacebookException error) {
                                     final ErrorInfo errorInfo;
                                     if (error == null) {
                                         final String postId = values.getString("post_id");
